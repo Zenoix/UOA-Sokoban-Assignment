da3c4731ee2368e8732e143f9ad65b7fc9e7df84
Zenoix <jeff.wang0296@gmail.com>
2021-09-29 21:52:54 +1300

    Completed docstring and started making the class

--- assignment.py
+++ assignment.py
@@ -1,14 +1,44 @@
 """
-Name:
-UPI:
-Description:
+Name: Jeff Wang
+UPI: jwan557
+Description: A command lines sokoban game created with Python where
+             a player must use wasd to push crates into holes.
 """
 
 
+from cgi import test
+
+
 class Sokoban:
-    """Your code here"""
+    def __init__(self, board):
+        self.__original_board = board
+        self.__board = board
 
+    def find_player(self):
+        for row in range(len(self.__board)):
+            for col in range(len(self.__board[0])):
+                if self.__board[row][col] == "P":
+                    return row, col
 
+    def complete(self):
+        pass
+
+    def get_steps(self):
+        pass
+
+    def restart(self):
+        self.__board = self.__original_board
+
+    def move(self, direction):
+        pass
+
+    def __str__(self):
+        output = ""
+        for row in self.__board:
+            output += " ".join(row) + "\n"
+        return output[:-1]
+
+'''
 def main(board):
     game = Sokoban(board)
     message = 'Press w/a/s/d to move, r to restart, or u to undo'
@@ -48,3 +78,15 @@ test_board = [
     ['*', '*', '*', '*', '*', '*', '*', '*']
 ]
 main(test_board)
+'''
+test_board = [
+    ['*', '*', '*', '*', '*', '*', '*', '*'],
+    ['*', ' ', ' ', ' ', ' ', ' ', ' ', '*'],
+    ['*', 'P', ' ', '#', ' ', ' ', ' ', '*'],
+    ['*', '*', '*', '*', '*', ' ', '#', '*'],
+    ['*', 'o', ' ', ' ', ' ', ' ', ' ', '*'],
+    ['*', ' ', ' ', ' ', ' ', ' ', 'o', '*'],
+    ['*', '*', '*', '*', '*', '*', '*', '*']
+]
+a = Sokoban(test_board)
+print(a)
