19137195490ac8f9dacb8831dee6b7e19961cffb
Zenoix <jeff.wang0296@gmail.com>
2021-09-29 21:21:27 +1300

    Uploaded specifications and template code

---
+++ assignment.py
@@ -0,0 +1,50 @@
+"""
+Name:
+UPI:
+Description:
+"""
+
+
+class Sokoban:
+    """Your code here"""
+
+
+def main(board):
+    game = Sokoban(board)
+    message = 'Press w/a/s/d to move, r to restart, or u to undo'
+    print(message)
+    while not game.complete():
+        print(game)
+        move = input('Move: ').lower()
+        while move not in ('w', 'a', 's', 'd', 'r', 'u'):
+            print('Invalid move.', message)
+            move = input('Move: ').lower()
+        if move == 'r':
+            game.restart()
+        elif move == 'u':
+            game.undo()
+        else:
+            game.move(move)
+    print(game)
+    print(f'Game won in {game.get_steps()} steps!')
+
+
+# This is here for you to test your code. You will need to test your code
+# yourself for this assignment. Remove any testing code (including the code
+# provided below) when you submit this file.
+#
+# The only code in your submission should be:
+#   - the Sokoban class
+#   - the main function
+#
+# There should be no other code included in your submission.
+test_board = [
+    ['*', '*', '*', '*', '*', '*', '*', '*'],
+    ['*', ' ', ' ', ' ', ' ', ' ', ' ', '*'],
+    ['*', 'P', ' ', '#', ' ', ' ', ' ', '*'],
+    ['*', '*', '*', '*', '*', ' ', '#', '*'],
+    ['*', 'o', ' ', ' ', ' ', ' ', ' ', '*'],
+    ['*', ' ', ' ', ' ', ' ', ' ', 'o', '*'],
+    ['*', '*', '*', '*', '*', '*', '*', '*']
+]
+main(test_board)
