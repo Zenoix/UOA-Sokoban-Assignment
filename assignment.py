301852f433fc5ccba2b2f8737ae561fec9a8c613
Zenoix <jeff.wang0296@gmail.com>
2021-10-03 03:28:46 +1300

    Made changes to methods regarding movement of player and crates

--- assignment.py
+++ assignment.py
@@ -12,7 +12,7 @@ class Sokoban:
         self.__width = len(board[0])
         self.__height = len(board)
         self.__num_of_holes = self.find_number_of_holes()
-        self.__game_history = []  # list containing codes for all actions
+        self.__game_log = []  # log containing all actions made in the game
         self.__player_moves = []  # coords for swaps when player moves
         self.__crate_moves = []  # swaps when crate move or goes in hole
 
@@ -38,25 +38,21 @@ class Sokoban:
         return len(self.__player_moves)
 
     def restart(self):
-        while self.__game_history:
+        while self.__game_log:
             self.undo()
 
     def undo(self):
-        if self.__game_history:
-            self.__game_history.pop()
-            swap_coords1, swap_coords2 = self.__player_moves.pop()
-            self.swap_squares(swap_coords1, swap_coords2)
-            if self.__game_history:
-                if self.__game_history[-1] == "crate_move":
-                    swap_coords1, swap_coords2 = self.__crate_moves.pop()
-                    self.swap_squares(swap_coords1, swap_coords2)
-                    self.__game_history.pop()
-                elif self.__game_history[-1] == "crate_in_hole":
-                    crate_coords, hole_coords = self.__crate_moves.pop()
-                    self.__board[crate_coords[0]][crate_coords[1]] = "#"
-                    self.__board[hole_coords[0]][hole_coords[1]] = "o"
-                    self.__num_of_holes += 1
-                    self.__game_history.pop()
+        # TODO fix undo method
+        if self.__game_log:
+            pass
+
+    def undo_crate_move(self):
+        # TODO implement
+        pass
+
+    def undo_crate_in_hole(self):
+        # TODO implement
+        pass
 
     def get_move_location(self, direction, initial_coords):
         initial_row, initial_col = initial_coords
@@ -72,39 +68,37 @@ class Sokoban:
 
     def move(self, direction):
         player_coords = self.find_player()
-        player_move_pos = self.get_move_location(
-            direction, player_coords)
+        player_move_coords = self.get_move_location(direction, player_coords)
+        if self.__board[player_move_coords[0]][player_move_coords[1]] == " ":
+            self.__game_log.append("player_move")
+            self.swap_squares(player_coords, player_move_coords)
+            swap_tuple = (player_coords, player_move_coords)
+            self.__player_moves.append(swap_tuple)
+        elif self.__board[player_move_coords[0]][player_move_coords[1]] == "#":
+            self.process_crate(direction, player_coords, player_move_coords)
 
-        if self.__board[player_move_pos[0]][player_move_pos[1]] == " ":
-            self.move_player(player_coords, player_move_pos)
-
-        elif self.__board[player_move_pos[0]][player_move_pos[1]] == "#":
-            self.move_crate(direction, player_coords, player_move_pos)
-
-    def move_player(self, player_coords, floor_coords):
-        self.swap_squares(player_coords, floor_coords)
-        swap_tuple = (player_coords, floor_coords)
-        self.__game_history.append("player_move")
-        self.__player_moves.append(swap_tuple)
-
-    def move_crate(self, direction, player_coords, crate_coords):
+    def process_crate(self, direction, player_coords, crate_coords):
         crate_move_coords = self.get_move_location(direction, crate_coords)
         crate_move_row, crate_move_col = crate_move_coords
         if self.__board[crate_move_row][crate_move_col] not in (" ", "o"):
             return
         elif self.__board[crate_move_row][crate_move_col] == " ":
+            self.__game_log.append("crate_move")
             self.swap_squares(crate_coords, crate_move_coords)
             swap_tuple = (crate_coords, crate_move_coords)
-            self.__game_history.append("crate_move")
             self.__crate_moves.append(swap_tuple)
         else:
-            self.__board[crate_move_row][crate_move_col] = " "
-            self.__board[crate_coords[0]][crate_coords[1]] = " "
+            self.__game_log.append("crate_in_hole")
+            self.crate_in_hole(crate_coords, crate_move_coords)
             crate_hole_tuple = (crate_coords, crate_move_coords)
-            self.__game_history.append("crate_in_hole")
             self.__crate_moves.append(crate_hole_tuple)
-            self.__num_of_holes -= 1
-        self.move_player(player_coords, crate_coords)
+        # move player after crate
+        self.swap_squares(player_coords, crate_coords) 
+
+    def crate_in_hole(self, crate_coords, hole_coords):
+        self.__board[hole_coords[0]][hole_coords[1]] = " "
+        self.__board[crate_coords[0]][crate_coords[1]] = " "
+        self.__num_of_holes -= 1
 
     def swap_squares(self, coord1, coord2):
         row1, col1 = coord1
@@ -117,7 +111,7 @@ class Sokoban:
         output = ""
         for row in self.__board:
             output += " ".join(row) + "\n"
-        # return the output with the last "\n" character
+        # return the output without the last "\n" character
         return output[:-1]
 
 
@@ -151,12 +145,12 @@ def main(board):
 #
 # There should be no other code included in your submission.
 test_board = [
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
+    ['*', '*', '*', '*', '*', '*', ' ', '*'],
     ['*', ' ', ' ', ' ', ' ', ' ', ' ', '*'],
-    ['*', 'P', ' ', '#', ' ', ' ', ' ', '*'],
+    [' ', 'P', ' ', '#', ' ', ' ', ' ', ' '],
     ['*', '*', '*', '*', '*', ' ', '#', '*'],
     ['*', 'o', ' ', ' ', ' ', ' ', ' ', '*'],
     ['*', ' ', ' ', ' ', ' ', ' ', 'o', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*']
+    ['*', '*', '*', '*', '*', '*', ' ', '*']
 ]
 main(test_board)
