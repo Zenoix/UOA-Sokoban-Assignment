40fb8e6de9b9a47190fc254c41307dd61610cd5d
Zenoix <jeff.wang0296@gmail.com>
2021-10-04 18:14:56 +1300

    Continued working on the move functionality. Did the logging feature, started undo

--- assignment.py
+++ assignment.py
@@ -14,7 +14,7 @@ class Sokoban:
         self.__num_of_holes = self.find_number_of_holes()
         self.__game_log = []  # log containing all actions made in the game
         self.__player_moves = []  # coords for swaps when player moves
-        self.__crate_moves = []  # swaps when crate move or goes in hole
+        self.__crate_moves = []  # coords for crate moves or crate in hole
 
     def find_number_of_holes(self):
         number_of_holes = 0
@@ -38,9 +38,16 @@ class Sokoban:
         return len(self.__player_moves)
 
     def undo(self):
-        # TODO fix undo method
+        # TODO finish undo method
         if self.__game_log:
-            pass
+            last_action = self.__game_log.pop()
+            if last_action == "player_move":
+                coord1, coord2 = self.__player_moves.pop()
+                self.swap_squares(coord1, coord2)
+            elif last_action == "crate_move":
+                self.undo_crate_move()
+            else:
+                self.undo_crate_in_hole()
 
     def undo_crate_move(self):
         # TODO implement
@@ -55,8 +62,15 @@ class Sokoban:
         while self.__game_log:
             self.undo()
 
-    def log_game_actions(self):
-        pass
+    def log_game_actions(self, action, coords_tuple):
+        if action == "player_move":
+            self.__player_moves.append(coords_tuple)
+            self.__game_log.append(action)
+        elif action == "crate_move" or action == "crate_in_hole":
+            self.__crate_moves.append(coords_tuple)
+            self.__game_log.append(action)
+        else:  # if action is None, the action is player taking a crate's spot
+            self.__player_moves.append(coords_tuple)  # logs empty moves
 
     def get_move_location(self, direction, initial_coords):
         initial_row, initial_col = initial_coords
@@ -81,10 +95,9 @@ class Sokoban:
         player_coords = self.find_player()
         player_move_coords = self.get_move_location(direction, player_coords)
         if self.__board[player_move_coords[0]][player_move_coords[1]] == " ":
-            self.__game_log.append("player_move")
             self.swap_squares(player_coords, player_move_coords)
             swap_tuple = (player_coords, player_move_coords)
-            self.__player_moves.append(swap_tuple)
+            self.log_game_actions("player_move", swap_tuple)
         elif self.__board[player_move_coords[0]][player_move_coords[1]] == "#":
             self.process_crate(direction, player_coords, player_move_coords)
 
@@ -93,23 +106,23 @@ class Sokoban:
         crate_move_row, crate_move_col = crate_move_coords
         if self.__board[crate_move_row][crate_move_col] not in (" ", "o"):
             return
-        elif self.__board[crate_move_row][crate_move_col] == " ":
-            self.__game_log.append("crate_move")
+        if self.__board[crate_move_row][crate_move_col] == " ":
             self.swap_squares(crate_coords, crate_move_coords)
             swap_tuple = (crate_coords, crate_move_coords)
-            self.__crate_moves.append(swap_tuple)
+            self.log_game_actions("crate_move", swap_tuple)
         else:
-            self.__game_log.append("crate_in_hole")
             self.crate_in_hole(crate_coords, crate_move_coords)
-            crate_hole_tuple = (crate_coords, crate_move_coords)
-            self.__crate_moves.append(crate_hole_tuple)
-        # move player after crate
+        # move player to the space left by crate
         self.swap_squares(player_coords, crate_coords)
+        # log the empty move's coordinates
+        self.log_game_actions(None, (player_coords, crate_coords))
 
     def crate_in_hole(self, crate_coords, hole_coords):
         self.__board[hole_coords[0]][hole_coords[1]] = " "
         self.__board[crate_coords[0]][crate_coords[1]] = " "
         self.__num_of_holes -= 1
+        crate_hole_tuple = (crate_coords, hole_coords)
+        self.log_game_actions("crate_in_hole", crate_hole_tuple)
 
     def __str__(self):
         output = ""
